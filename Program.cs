c8aac40d35a38988a0d644053cfc6ca582deab3c
bramiller1989 <bramiller1989@gmail.com>
2021-01-18 21:08:50 -0600

    add float in string temp

--- Program.cs
+++ Program.cs
@@ -21,6 +21,8 @@ namespace Variables
             Console.WriteLine(isWorking);
 
             Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
         }
     }
 }
