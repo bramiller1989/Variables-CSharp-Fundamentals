f6dc20511b3e6b9e065258298265cb5de611ee7b
bramiller1989 <bramiller1989@gmail.com>
2021-01-18 20:37:43 -0600

    remove explicit type and use implicit var

--- Program.cs
+++ Program.cs
@@ -6,12 +6,12 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            byte number = 2;
-            int count = 10;
-            float totalPrice = 20.95f;
-            char character = 'A';
-            string firstName = "Branden";
-            bool isWorking = true;
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Branden";
+            var isWorking = true;
 
             Console.WriteLine(number);
             Console.WriteLine(count);
